d9ca4df002e96364a5889a95f626e00d9eee39be
BenedettiGianluca <99gian.bene@gmail.com>
2022-02-09 12:11:46 +0100

    Header (senza props)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <Nav />
+    <nav>
+      <div>
+        <img src="./assets/project_images/avada-charity-logo.png">
+      </div>
+      <div>
+        <li>HOME</li>
+        <li>MISSION</li>
+        <li>CAUSES</li>
+        <li>JOURNAL</li>
+        <button type="button">DONATE</button>
+      </div>
+    </nav>
     <Header />
     <Main />
     <Footer />
@@ -8,7 +19,6 @@
 </template>
 
 <script>
-  import Nav from './components/Nav.vue';
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
   import Footer from './components/Footer.vue';
@@ -16,7 +26,6 @@
   export default {
     name: 'App',
     components: {
-      Nav,
       Header,
       Main,
       Footer
@@ -25,9 +34,57 @@
 </script>
 
 <style lang="scss">
+  @import "./assets/scssVariables.scss";
+
   * {
     box-sizing: border-box;
     border: 0;
     margin: 0;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 10px;
+  }
+
+  h1 {
+    font-size: 38px;
+  }
+
+  p {
+    font-size: 15px;
+  }
+
+  li:hover, button:hover {
+    cursor: pointer;
+  }
+
+  nav {
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 88vw;
+    margin: 3vh 9.5vw 0 2.5vw;
+
+    img {
+      transform: scale(0.66);
+    }
+
+    li {
+      display: inline;
+      color: $FrenchGray;
+      margin: 0 30px;
+
+      &:hover {
+        color: $Goldenrod;
+      }
+    }
+
+    button {
+      background-color: $Goldenrod;
+      color: white;
+      padding: 9px 18px;
+      border: 1px solid $Goldenrod;
+      margin-left: 30px;
+    }
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
   <header>
+
+    <section>
+      <h1>MAKE A DIFFERENCE</h1>
+      <p>As long as poverty, injustice & inequality persist, none of us can truly rest</p>
+      <div>
+        <button type="button">OUR MISSION</button>
+        <button type="button">DONATE NOW</button>
+      </div>
+    </section>
   </header>
 </template>
 
@@ -10,5 +19,39 @@
 </script>
 
 <style lang="scss">
+  @import "../assets/scssVariables.scss";
 
+  header {
+    background-image: url('../assets/project_images/home-page-slider.jpg');
+    background-size: cover;
+    background-position: center;
+    height: 105vh;
+  
+    section {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      padding-top: 40vh;
+
+      p {
+        color: $FrenchGray;
+        margin: 15px 0 30px 0;
+      }
+
+      button {
+        background-color: rgba(0,0,0,0.0);
+        color: $FrenchGray;
+        padding: 9px 18px;
+        border: 1px solid white;
+        margin: 0 5px;
+
+        &:hover {
+          color: $Goldenrod;
+          border-color: $Goldenrod;
+        }
+      }
+    }
+  }
 </style>
